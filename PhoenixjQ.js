c64e293e9a8d51d95f9d3eb81c0916842f110be9
TechnArtistica <sarah.adina.equestrian@gmail.com>
2017-09-25 21:36:33 -0500

    Initial commit

---
+++ PhoenixjQ.js
@@ -0,0 +1,20 @@
+
+$("#contactBtn").click(function(){
+    alert("Thank you, your information has been submitted! Someone will contact you soon.");
+});
+
+$('.lessons').click(function(){
+    $('.learn').toggle();
+});
+
+$('.camp').click(function(){
+    $('.fun').toggle();
+});
+
+$('.training').click(function(){
+    $('.ride').toggle();
+});
+
+$('.barn').click(function(){
+    $('.live').toggle();
+});
